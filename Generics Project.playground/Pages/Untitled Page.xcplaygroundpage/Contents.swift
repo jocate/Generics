1ca7668a08cf50f048da7cc7cb8887dd61d5999d
Jocelyn Stuart <luluscomputer@jocelyns-macbook-air.local>
2019-02-19 19:30:19 -0500

    adds completed countedSet project

---
+++ Generics Project.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,74 @@
+import UIKit
+
+struct CountedSet<Element> : ExpressibleByArrayLiteral where Element : Hashable {
+    
+    private var elementDict: [Element: Int] = [:]
+    
+    init(arrayLiteral elements: Element...) {
+        
+        for e in elements {
+            self.insert(type: e)
+        }
+       
+    }
+ 
+  
+    init(elementDict: [Element: Int]) {
+        self.elementDict = elementDict
+       
+    }
+    
+    func printOut() {
+        print(elementDict)
+        print(CountedSet<Element>.self)
+    }
+  
+   
+    // Insert
+    
+    mutating func insert(type: Element) {
+        
+        if let value = elementDict[type] {
+            elementDict[type] = value + 1
+        } else {
+            elementDict[type] = 1
+        }
+    }
+    
+ 
+    // Remove
+    
+    mutating func remove(type: Element) -> Int {
+        if let value = elementDict[type] {
+            elementDict[type] = value - 1
+            return elementDict[type]!
+        } else {
+            return 0
+        }
+    }
+    
+    subscript(_ member: Element) -> Int {
+        if let value = elementDict[member] {
+            return value
+        } else {
+            return 0
+        }
+    }
+    
+    // Subscripting
+    /*mutating func subscript(_ member: Element) -> Int {
+        
+    }*/
+}
+
+enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
+var aCountedSet = CountedSet<Arrow>()
+aCountedSet.printOut()
+aCountedSet[.iron] // 0
+var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+myCountedSet[.iron] // 4
+myCountedSet.remove(type: .iron) // 3
+myCountedSet.remove(type: .dwarvish) // 0
+myCountedSet.remove(type: .magic) // 0
+myCountedSet.insert(type: .elven)
+myCountedSet[.elven]
